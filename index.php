e54b68c7870ec6db3271b78b824c9464a9c37060
ashutoshtech <ashutosh.k@techmatrixconsulting.com>
2023-07-13 12:02:05 +0530

    New Code insert

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>City Energy</title>
+
+  <link rel="stylesheet" href="common/css/style.css">
+  <link rel="stylesheet" href="common/css/responsive.css">
+   <link rel="stylesheet" href="common/css/bootstrap.min.css">
+  <link rel="stylesheet" href="common/webfonts/all.css">
+  <link rel="stylesheet" href="common/fonts/font.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script text="javascript" src="common/js/bootstrap.min.js"></script>
+
+
+  
+  
+</head>
+<body>
+<section class="home">
+
+<header>
+  <div class="container inner">
+  <a href="Home.html" class="logo"><img src="common/images/logo.png" alt="logo"></a>
+
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <img class="profile-icon" src="common/images/profile-icon.png" alt=""> Cindy
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <span>Signed as in Cindy</span>
+    <a class="dropdown-item" href="profile.html"><i class="fas fa-user"></i> My Profile</a>
+    <a class="dropdown-item" href="change-password.html"><i class="fas fa-unlock-alt"></i> Change Password</a>
+    <a class="dropdown-item" href="#"><i class="far fa-power-off"></i> Logout</a>
+  </div>
+</div>
+</div>
+</header>
+
+
+<section class="home-banner">
+<div class="container">
+<div class="row">
+  <div class="col-lg-6 col-md-6 col-sm-12">
+<div class="left-text">
+<span class="sub-heading">Welcome to</span>
+<h2><span>City</span>Energy</h2>
+<p>City Energy supplies piped town gas nationwide. We are also providing innovative green energy solutions, such as green hydrogen, IoT-enabled low-carbon solutions and electric vehicle charging services.
+</p>
+
+</div>
+</div>
+
+<div class="col-lg-6 col-md-6 col-sm-12">
+<div class="right-graphics">
+<img class="circle-img" src="common/images/circle-img.png" alt="">
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<section class="appointment-container">
+<div class="container">
+  <div class="top-cols">
+    <div class="row">
+    <div class="col-md-3">
+  <div class="appointment-box">
+    <ul>
+      <li><a href="#"><img src="common/images/Sp-account.png" alt=""></a>Add an SP Account</li>
+    </ul>
+    <p>Add a new SP account to access Installation and Service Operations</p>
+    <a target="_blank" href="add-sp-account.html" class="next-arrow"><i class="far fa-arrow-right"></i></a>
+    </div>
+</div>
+<div class="col-md-3">
+<div class="appointment-box">
+<ul>
+  <li><a href="#"><img src="common/images/calendar.png" alt=""></a>Make an Appointment</li>
+</ul>
+<p>Make an appointment for gas pipe installation and supply turn-on.</p>
+<a target="_blank" href="make-an-appointment.html" class="next-arrow"><i class="far fa-arrow-right"></i></a>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="appointment-box">
+  <ul>
+    <li><a href="#"><img src="common/images/create-case.png" alt=""></a>Create Case</li>
+  </ul>
+  <p>Lorem Ipsum is simply dummy text</p>
+  <a target="_blank" href="create-case.html" class="next-arrow"><i class="far fa-arrow-right"></i></a>
+  </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="appointment-box">
+      <ul>
+        <li><a href="#"><img src="common/images/history.png" alt=""></a>Appointment/ Case History</li>
+      </ul>
+      <p>You can see your all appointment history</p>
+      <a target="_blank" href="Case-History.html" class="next-arrow"><i class="far fa-arrow-right"></i></a>
+      </div>
+      </div>
+
+
+</div>
+</div>
+
+  <div class="view-appointment">
+    <h5>View Appointments</h5>
+  <div class="appointment-box full">
+   <table class="appointment-data">
+    <tbody>
+      <tr>
+        <td><b>Type of Appointment :</b></td>
+        <td>Register for phone consultation for greener and smarter solutions</td>
+      </tr>
+      <tr>
+        <td><b>Gas Dryer Installation:</b></td>
+        <td>RDT-62-SG-W</td>
+      </tr>
+      <tr>
+        <td><b>Time and Date :</b></td>
+        <td>16-11-2022, 06:00 PM</td>
+      </tr>
+    </tbody>
+   </table>
+
+   <ul class="btn-set">
+    <li><button class="fill-btn">Reschedule</button></li>
+    <li><button class="border-btn">Cancel</button></li>
+   </ul>
+    
+    </div>
+  </div>
+
+</div>
+</section>
+
+<section class="bottom-container">
+  <div class="container">
+    <h4>Good Energy for Our City</h4>
+    <p>From piped town gas to electric vehicle (EV) charging; from lifestyle energy solutions to exploring new energy <br>solutions – we are inspiring smart and earth-friendly ways of living and working every day.
+    </p>
+  </div>
+</section>
+
+<footer>
+  <div class="conatiner">
+   <ul>
+    <li><a href="#">Privacy Policy</a></li>
+    <li><a href="#">terms and conditions</a></li>
+   </ul>
+  </div>
+</footer>
+
+
+
+</section>
+
+</body>
+</html>
